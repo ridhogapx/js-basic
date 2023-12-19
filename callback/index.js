e7239f2966f404f0ecc67a344e8415b56d9d6480
RageNeko26 <rneko2006@gmail.com>
2023-12-19 14:08:58 +0700

    feat: Add callback and closure functions

---
+++ callback/index.js
@@ -0,0 +1,51 @@
+// Apa itu callback dalam Javascript
+// Memanggil functions di dalam functions 
+
+// Contoh: 1
+function perkalian(a, b) {
+  return a * b
+}
+
+function luasPersegi(sisi) { 
+  const luas = perkalian(sisi, sisi)
+  return luas
+}
+
+console.log(luasPersegi(4))
+
+// Contoh: 2
+function pertambahan(a, b) {
+  return a + b
+}
+
+function kelilingPersegiPanjang(panjang, lebar, callbackPerkalian, callbackPertambahan) {
+  // Function Mencari keliling persegi panjang
+  
+  // 2 * p + l
+
+  // Callback 
+  let keliling = callbackPertambahan(panjang, lebar) 
+
+  return callbackPerkalian(keliling, 2)
+}
+
+console.log(`Hasil keliling persegi:` + kelilingPersegiPanjang(10, 5, perkalian, pertambahan))
+// Ekspetasi Output: 30
+
+// Contoh: 3 
+function greeting() {
+  return "Helo, good morning!"
+}
+
+function tampilkan(callbackGreeting) {
+  let greeting = callbackGreeting()
+  console.log(`Tampilkan pesan: ` + greeting)
+}
+
+
+// Contoh Closure
+tampilkan(function() {
+  return "Hello dari Closure!"
+})
+
+
