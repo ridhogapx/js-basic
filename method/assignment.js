9014b3435fa529a15610aed6d652184b07444a8a
RageNeko26 <rneko2006@gmail.com>
2023-12-28 12:59:32 +0700

    feat: Add daily assignment

---
+++ method/assignment.js
@@ -0,0 +1,20 @@
+// Assignment: Method and keyword 'this'
+// Kerjakan dalam format file study-case.js
+
+// #1 Lengkapilah kode di bawah ini.
+const tiger = {
+    name: "Daniel",
+    fur: "Orange",
+    food: ["Raw Chicken Meat", "Beef"]
+    // Buatlah sebuah method untuk me-return property 'foods'
+    // Method dinamai dengan nama getFood()
+    // Return harus dalam bentuk string bukan array. Sehingga memerlukan loop untuk mengeluarkan datanya
+    // Hasil return harus berupa format: 'Daniel likes ' + food
+}
+
+// #2 Buatlah object sebuah burung Elang / Eagle
+// Dengan ketentuan property object:
+// name, fur, food, voice(), 
+const eagle = {
+    // Lengkapilah kode di bawah ini.
+}
